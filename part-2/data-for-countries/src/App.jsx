a47c634bf8abd57f5e61b37457a1ab414db27113
mushkan27 <muskanyon@gmail.com>
2025-04-28 14:41:11 +0545

    Exe-2.20: Add weather data fetching and display in CountryDetail component(Part-2 Exercise Completed)

--- part-2/data-for-countries/src/App.jsx
+++ part-2/data-for-countries/src/App.jsx
@@ -9,6 +9,8 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchItem, setSearchItem] = useState('');
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [apiError, setApiError] = useState(null);
 
   useEffect(()=>{
     if(searchItem.trim()===""){
@@ -19,8 +21,16 @@ const App = () => {
       try {
         const url = `https://restcountries.com/v3.1/name/${searchItem}`;
         const response = await axios.get(url);
-        console.log(response.data)
+        // console.log("responseData",response.data)
         setCountries(response.data);
+        setSelectedCountry(null);
+        setWeather(null);
+        if(response.data.length === 1){
+         const capital = Array.isArray(response.data[0].capital) ? response.data[0].capital[0] : response.data[0].capital;
+        //  console.log("capital:", capital)
+        //  console.log("responseData at 0:",response.data[0])
+         fetchWeatherData(capital)
+        }
         
       } catch (error) {
         console.log("Error fetching data:", error)
@@ -30,6 +40,28 @@ const App = () => {
 
   },[searchItem]);
 
+
+  const fetchWeatherData = async (capital) => {
+    // console.log("capita inside api",capital)
+    // console.log("inside the api key")
+    try {
+      const apiKey = import.meta.env.VITE_SOME_KEY;
+      // console.log(apiKey, "apikey")
+      const v2_5 = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`;
+      // console.log(v2_5,"api call")
+      
+      const weatherResponse = await axios.get(v2_5)
+      // console.log("weatherResponse",weatherResponse)
+      // console.log("weatherResponseData:", weatherResponse.data)
+      setWeather(weatherResponse.data)
+      setApiError(null);
+      
+    } catch (error) {
+      console.error("Error fetching the weather data", error.data)
+      setWeather(null)
+      setApiError("failed to fetch weather data")
+    }
+  }
   
 
   const handleSearch = (e) => {
@@ -39,6 +71,8 @@ const App = () => {
 
   const handleShow = (country)=>{
     setSelectedCountry(country)
+    const capital = Array.isArray(country.capital) ? country.capital[0] : country.capital;
+    fetchWeatherData(capital)
   }
 
 
@@ -46,7 +80,7 @@ const App = () => {
     <>
   <SearchBar searchItem={searchItem} handleSearch={handleSearch}/>
   {selectedCountry? (
-    <CountryDetail country={selectedCountry}/>
+    <CountryDetail country={selectedCountry} weather={weather} apiError={apiError}/>
   ): (
     <>
   {countries.length > 10 && (
@@ -58,7 +92,7 @@ const App = () => {
   )}
 
   {countries.length === 1 && (
-    <CountryDetail country={countries[0]}  />
+    <CountryDetail country={countries[0]} weather={weather} apiError={apiError} />
   )}
   </>
 )}
--- part-2/data-for-countries/src/components/CountryDetail.jsx
+++ part-2/data-for-countries/src/components/CountryDetail.jsx
@@ -3,7 +3,7 @@
 //When only one country matches the search, your API result looks like an array containing one object.
 //So to show the details of that one country, you need to access the first item in the array → that's at index 0.
 
-const CountryDetail = ({ country }) => {
+const CountryDetail = ({ country,weather,apiError }) => {
     
     const languages = country.languages ? Object.values(country.languages) : [];
   
@@ -21,6 +21,30 @@ const CountryDetail = ({ country }) => {
 
         <h3>Flag:</h3>
         <img src={country.flags.png} alt={`${country.name.common}'s flag`} />
+
+        <h3>Weather Map Data</h3>
+        {
+            weather ? (
+                <div>
+                    <h3>Weather in {country.capital[0]}</h3>
+                    <p>Temperature: {weather.main.temp}°C</p>
+                    <p>Humidity: {weather.main.humidity}</p>
+                    <p>Wind {weather.wind.speed}</p>
+                    <p>Weather Description: {weather.weather[0].description}</p>
+                    <p>Weather Icon: </p>
+                    {
+                      weather.weather[0].icon ?(
+                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="Weather Icon" />
+                      ):(
+                        <p>No icon available</p>
+                      )
+                    }
+                </div>
+            ):apiError ? (<p>{apiError}</p>):(
+              <p>Loading weather data</p>
+            )
+        }
+         
       </div>
     );
   }
