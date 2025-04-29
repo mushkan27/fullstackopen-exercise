05e9712b65eed80e7309174cde6acecfdf339934
mushkan27 <muskanyon@gmail.com>
2025-04-29 16:53:57 +0545

    Exe-3.2: Add info endpoint to display phonebook details and current date

--- part-3/persons/index.js
+++ part-3/persons/index.js
@@ -35,7 +35,10 @@ app.get("/api/persons",(request, response) => {
     response.json(persons)
   })
 
-  
+  app.get("/info",(request, response)=>{
+    response.send(`Phonebook has info for ${persons.length} people. <br> ${Date()}`)
+  })
+
 
 const PORT = 3001
 app.listen(PORT)
