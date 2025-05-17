a1c59030c19a7bd175d359d7c72c3a1d76dffb6e
mushkan27 <muskanyon@gmail.com>
2025-05-17 10:05:36 +0545

    feat: enhance phone number validation in person schema

--- part-3/persons/index.js
+++ part-3/persons/index.js
@@ -25,7 +25,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v){
+        return /^\d{2,3}-\d+$/.test(v) && v.length >=8
+      },
+      message: (props)=>`${props.value} is not a valid phone number`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
