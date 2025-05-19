38c2e6820ce6805794e7f1aa383a4dedd3e489df
mushkan27 <muskanyon@gmail.com>
2025-05-19 14:02:51 +0545

    fix: fix errors in mongo.js file

--- part-3/persons/mongo.js
+++ part-3/persons/mongo.js
@@ -23,30 +23,30 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 if(name && number){
-    const person = new Person({
-        name,
-        number,
-       })
-       
-       person.save().then(result => {
-         console.log(`added ${name} number ${number} to phonebook`)
-         mongoose.connection.close()
-       })
-       .catch((err)=>{
-        console.error("Error saving the person:",err)
-        mongoose.connection.close()
-       })
-       
-      
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+    .catch((err) => {
+      console.error('Error saving the person:',err)
+      mongoose.connection.close()
+    })
+
+
 } else{
-    Person.find({}).then((result)=> {
-        console.log("phonebook:")
-        result.forEach((person)=>{
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
-    }).catch((err)=>{
-        console.error("Error saving the person:",err)
-        mongoose.connection.close()
-       })
+  Person.find({}).then((result) => {
+    console.log('phonebook:')
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  }).catch((err) => {
+    console.error('Error saving the person:',err)
+    mongoose.connection.close()
+  })
 }
