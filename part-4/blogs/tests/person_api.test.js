5af542611fe87438a942c65b8084624da860b141
mushkan27 <muskanyon@gmail.com>
2025-05-26 21:01:01 +0545

    test(api): add GET /api/blogs tests for JSON response and blog count

--- part-4/blogs/tests/person_api.test.js
+++ part-4/blogs/tests/person_api.test.js
@@ -1,9 +1,33 @@
-const { test, after, describe } = require('node:test')
+const assert = require('node:assert')
+const { test, after, describe, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "First blog",
+    author: "author 1",
+    url: 'http://test1.com',
+    likes: 1
+  },
+  {
+    title: "Second blog",
+    author: "author 2",
+    url: 'http://test2.com',
+    likes: 2
+  }
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 //testing get method
 describe('testing GET method', () => {
@@ -13,10 +37,10 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
-test('all notes are returned', async () => {
-  const response = await api.get('/api/notes')
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 })
 
