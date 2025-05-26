0083d4235f4e53b19c648e7b5f785d170c252a19
mushkan27 <muskanyon@gmail.com>
2025-05-26 20:23:36 +0545

    feat: update environment configuration and add cross-env for script compatibility

--- part-4/blogs/utils/config.js
+++ part-4/blogs/utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = process.env.NODE_ENV === 'test' 
+? process.env.TEST_MONGODB_URI
+: process.env.MONGODB_URI
 const PORT = process.env.PORT || 3003
 
-module.exports = { mongoUrl,PORT }
+module.exports = { 
+    MONGODB_URI: mongoUrl,
+    PORT }
