d00e7799f26d29472e61725f6cfdccee6cb81114
mushkan27 <muskanyon@gmail.com>
2025-05-26 20:17:32 +0545

    feat: add supertest for API testing

---
+++ part-4/blogs/tests/person_api.test.js
@@ -0,0 +1,20 @@
+const { test, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+//testing get method
+describe('testing GET method', () => {
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
