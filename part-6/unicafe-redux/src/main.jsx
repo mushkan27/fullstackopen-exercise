c54fd159773f8bb202c02db526059b576c86dced
mushkan27 <muskanyon@gmail.com>
2025-07-09 15:20:55 +0545

    feat: implement feedback reducer with GOOD, OK, BAD, and ZERO actions test: add reducer tests for initial state and action handling

--- part-6/unicafe-redux/src/main.jsx
+++ part-6/unicafe-redux/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
 import { createStore } from 'redux'
 import reducer from './reducer'
@@ -12,21 +12,41 @@ const App = () => {
       type: 'GOOD'
     })
   }
+  console.log(store.getState())
+
+  const bad = () => {
+    store.dispatch({
+      type:'BAD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type:'OK'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
 
   return (
     <div>
       <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
 
 const renderApp = () => {
   root.render(<App />)
@@ -34,3 +54,4 @@ const renderApp = () => {
 
 renderApp()
 store.subscribe(renderApp)
+
