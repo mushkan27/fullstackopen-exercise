0961d9c20e232a2c3a6052e572dbe0cce6fcadf9
mushkan27 <muskanyon@gmail.com>
2025-07-10 12:00:28 +0545

    sort anecdotes by no. of votes

--- part-6/redux-anecdotes/src/App.jsx
+++ part-6/redux-anecdotes/src/App.jsx
@@ -1,8 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector(state =>
+    [...state].sort((a, b) => b.votes - a.votes)
+  )
+    const dispatch = useDispatch()
 
   const vote = (id) => {
     console.log('vote', id)
