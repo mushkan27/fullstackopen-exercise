2fbc2d9b4f9f9cb31dba6154a466145ec0e96cba
mushkan27 <muskanyon@gmail.com>
2025-07-10 12:30:22 +0545

    feat: extract anecdote list into AnecdoteList component

--- part-6/redux-anecdotes/src/App.jsx
+++ part-6/redux-anecdotes/src/App.jsx
@@ -1,33 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { vote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state =>
-    [...state].sort((a, b) => b.votes - a.votes)
-  )
-    const dispatch = useDispatch()
-
-  const voteAnecdote = (id) => {
-    dispatch(vote(id))
-  }
-
- 
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => voteAnecdote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ part-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+    const anecdotes = useSelector(state =>
+        [...state].sort((a, b) => b.votes - a.votes)
+    )
+    const dispatch = useDispatch()
+
+    const voteAnecdote = (id) => {
+        dispatch(vote(id))
+    }
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => voteAnecdote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
