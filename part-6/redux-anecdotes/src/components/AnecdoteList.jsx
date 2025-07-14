3f2d2f449629d3716f892b8177e1fcb41c0a2603
mushkan27 <muskanyon@gmail.com>
2025-07-14 11:23:20 +0545

    feat: add filter functionality to anecdotes and update reducers

--- part-6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,9 +2,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state =>
-        [...state].sort((a, b) => b.votes - a.votes)
-    )
+    const anecdotes = useSelector(({ anecdotes, filter }) => {
+        return anecdotes
+          .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+          .sort((a, b) => b.votes - a.votes)
+      })
     const dispatch = useDispatch()
 
     const voteAnecdote = (id) => {
