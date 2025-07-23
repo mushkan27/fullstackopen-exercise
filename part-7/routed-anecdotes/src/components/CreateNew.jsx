627fef16c5dc6dd58a7ae8b18aec0ee1d1251d0c
mushkan27 <muskanyon@gmail.com>
2025-07-23 11:44:37 +0545

    feat: refactor CreateNew component to use custom useField hook for form handling

--- part-7/routed-anecdotes/src/components/CreateNew.jsx
+++ part-7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = ({ addNew, setNotification }) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    setNotification(`A new anecdote "${content}" created!`)
+    setNotification(`A new anecdote "${content.value}" created!`)
     setTimeout(() => {
       setNotification('')
     }, 5000)
@@ -29,15 +29,15 @@ const CreateNew = ({ addNew, setNotification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name={content.name} value={content.value} onChange={content.onChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name={author.name} value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name={info.name} value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
       </form>
