f5caf2d00320d386961e37333bc691cf3086e596
mushkan27 <muskanyon@gmail.com>
2025-08-05 11:35:30 +0545

    feat: implement Blog model and update API endpoint to use Sequelize ORM for fetching blogs

--- part-13/blogs-server/cli.js
+++ part-13/blogs-server/cli.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
 const express = require('express')
 const app = express()
@@ -14,8 +14,38 @@ const sequelize = new Sequelize(process.env.DB_URL, {
   },
 });
 
+class Blog extends Model {}
+
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.STRING
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
 app.get('/api/blogs', async(req, res) => {
-    const blogs = await sequelize.query("SELECT * from blogs", { type: QueryTypes.SELECT })
+    const blogs = await Blog.findAll()
     res.json(blogs)
 })
 
