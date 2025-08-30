c08c919a9d9c944998b7bfa0217a52bc37291d84
mushkan27 <muskanyon@gmail.com>
2025-08-30 10:59:58 +0545

    feat: implement BMI calculation functionality and add start script for execution

---
+++ part-9/ts-project/bmiCalculator.ts
@@ -0,0 +1,19 @@
+
+const calculateBmi = (height:number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+
+  if (bmi < 18.5) {
+    return "Underweight range";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal range";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return "Overweight range";
+  } else {
+    return "Obese range";
+  }
+};
+
+
+
+console.log(calculateBmi(180, 74))
