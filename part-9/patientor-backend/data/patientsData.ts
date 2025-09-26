5095afff11082882a384cadb48ad4444ef116a09
mushkan27 <muskanyon@gmail.com>
2025-09-26 09:27:05 +0545

    feat: update patientsData to use Gender enum for gender field

--- part-9/patientor-backend/data/patientsData.ts
+++ part-9/patientor-backend/data/patientsData.ts
@@ -1,4 +1,4 @@
-import { Patient } from '../src/types';
+import { Patient, Gender } from '../src/types';
 
 
 const patientsData: Patient[] = [
@@ -7,7 +7,7 @@ const patientsData: Patient[] = [
         "name": "John McClane",
         "dateOfBirth": "1986-07-09",
         "ssn": "090786-122X",
-        "gender": "male",
+        "gender": Gender.Male,
         "occupation": "New york city cop"
     },
     {
@@ -15,7 +15,7 @@ const patientsData: Patient[] = [
         "name": "Martin Riggs",
         "dateOfBirth": "1979-01-30",
         "ssn": "300179-77A",
-        "gender": "male",
+        "gender": Gender.Male,
         "occupation": "Cop"
     },
     {
@@ -23,7 +23,7 @@ const patientsData: Patient[] = [
         "name": "Hans Gruber",
         "dateOfBirth": "1970-04-25",
         "ssn": "250470-555L",
-        "gender": "other",
+        "gender": Gender.Other,
         "occupation": "Technician"
     },
     {
@@ -31,7 +31,7 @@ const patientsData: Patient[] = [
         "name": "Dana Scully",
         "dateOfBirth": "1974-01-05",
         "ssn": "050174-432N",
-        "gender": "female",
+        "gender": Gender.Female,
         "occupation": "Forensic Pathologist"
     },
     {
@@ -39,7 +39,7 @@ const patientsData: Patient[] = [
         "name": "Matti Luukkainen",
         "dateOfBirth": "1971-04-09",
         "ssn": "090471-8890",
-        "gender": "male",
+        "gender": Gender.Male,
         "occupation": "Digital evangelist"
     }
 ];
